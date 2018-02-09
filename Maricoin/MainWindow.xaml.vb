e006b93d57f9ed0f3acaab8cf76014609c8f1eb2
TheLBall <outofmario1000@gmail.com>
2018-02-09 17:31:17 -0500

    Add project files.

---
+++ Maricoin/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Try
+            Dim finalscore
+            finalscore = coinno.Text Mod 256
+            coinscore.Content = finalscore
+        Catch ex As Exception
+            MessageBox.Show("Something happened. Please try again.", "Maricoin: Something happened")
+        End Try
+    End Sub
+End Class
